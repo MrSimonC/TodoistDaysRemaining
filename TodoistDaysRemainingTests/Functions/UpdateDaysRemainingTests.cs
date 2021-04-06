4cf369fe5ecee604da9ff511749588f2cd2f0589
Simon Crouch <simon.crouch@felinesoft.com>
2021-04-06 22:51:18 +0100

    remove tests into main solution directory

---
+++ TodoistDaysRemainingTests/Functions/UpdateDaysRemainingTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todoist.Net;
+using Todoist.Net.Models;
+
+namespace TodoistDaysRemaining.Functions.Tests
+{
+    [TestClass()]
+    public class UpdateDaysRemainingTests
+    {
+        ITodoistClient client = null;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            client = new TodoistClient(Environment.GetEnvironmentVariable("TODOIST_APIKEY"));
+
+        }
+
+        [TestMethod()]
+        public void CalculateDaysTomorrowTest()
+        {
+            DateTime tomorrow = DateTime.Now.Date.AddDays(1);
+            (int days, int _) = UpdateDaysRemaining.CalculateDays(tomorrow);
+            Assert.AreEqual(1, days);
+        }
+
+        [TestMethod()]
+        public void UtcDateTest()
+        {
+            var dueDateUtc = DateTime.UtcNow.Date.AddDays(5);
+            var item = new Item("simon demo task") { DueDate = new DueDate(DateTime.Parse("2021-04-10")) };
+            client.Items.AddAsync(item).Wait();
+
+            var itemInfo = client.Items.GetAsync(item.Id).Result;
+
+            Assert.AreEqual(dueDateUtc, itemInfo.Item.DueDate);
+        }
+    }
+}
