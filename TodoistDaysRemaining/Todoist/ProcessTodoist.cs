b84956c35ebfc081d29c2e52da99f8d54fa6eafb
Simon C <iamontheinternet@yahoo.com>
2022-01-29 22:53:05 +0000

    refactor out workday display choice and nullable boolean

--- TodoistDaysRemaining/Todoist/ProcessTodoist.cs
+++ TodoistDaysRemaining/Todoist/ProcessTodoist.cs
@@ -16,32 +16,21 @@ public class ProcessTodoist
     public ProcessTodoist()
     {
         TodoistAPIKey = Environment.GetEnvironmentVariable("TODOIST_APIKEY") ?? throw new NullReferenceException("Missing TODOIST_APIKEY environment variable");
-        string completePastItemsString = Environment.GetEnvironmentVariable("COMPLETE_PAST_ITEMS") ?? throw new NullReferenceException("Missing COMPLETE_PAST_ITEMS environment variable");
-        if (!bool.TryParse(completePastItemsString, out CompletePastItems))
-        {
-            throw new ArgumentException(nameof(completePastItemsString));
-        }
-        CompletePastItems = bool.Parse(completePastItemsString);
+        CompletePastItems = GetBoolFromEnvVar("COMPLETE_PAST_ITEMS");
     }
 
     public async Task ProcessTodoistAsync(ILogger log)
     {
         ITodoistClient client = new TodoistClient(TodoistAPIKey);
         // Force writing to all todoist entries (change from true to false directly in Portal > Configuration since SetEnvironmentVariable won't work in Azure Functions
-        const string forceWriteOnceEnvVarName = "FORCE_WRITE_ONCE";
-        bool forceWrite = GetBoolFromEnvVar(forceWriteOnceEnvVarName) ?? false;
+        bool forceWrite = GetBoolFromEnvVar("FORCE_WRITE_ONCE");
 
         List<string> todoistProjectsToTraverse = GetListOfProjectsFromConfig(log);
         List<ComplexId> todoistProjectIds = await GetTodoistProjectIds(log, todoistProjectsToTraverse, client);
         List<Item> todoistItemsToProcess = await GetTodoistProjectItems(log, client, todoistProjectIds);
 
-        // traverse each item for existing Regex
+        // Group 1: Total normal days remaining
         string regex = @"\ +\[+(\d+)\/*\d*\ days\ remaining\]+";
-        // See if we want to only count work days. If null, then include both all days and work days.
-        // true=yes workdays only, false=all days, null=show both
-        bool? workWeekOnly = GetBoolFromEnvVar("WORKWEEK");
-        log.LogInformation($"WorkWeekOnly set to {workWeekOnly}");
-
         foreach (Item item in todoistItemsToProcess.Where(i => i?.DueDate?.Date.HasValue ?? false))
         {
             if (item.DueDate.Date!.Value.ToUniversalTime() < DateTime.UtcNow)
@@ -51,8 +40,7 @@ public class ProcessTodoist
                 {
                     log.LogInformation($"CompletePastItems is true. Complete Item: {item.Content}.");
 #if !DEBUG
-                    var completeItemArg = new CompleteItemArgument(item.Id, DateTime.UtcNow);
-                    await client.Items.CompleteAsync(completeItemArg);
+                    await client.Items.CloseAsync(item.Id);
 #endif
                     continue;
                 }
@@ -69,15 +57,13 @@ public class ProcessTodoist
                 try
                 {
                     Match m = Regex.Match(item.Content, regex);
-                    bool gotExistingDays = int.TryParse(m.Groups[1]?.Value, out int existingDays); // ...[12/34 or ...[12..
-                    if (!gotExistingDays)
+                    if (!int.TryParse(m.Groups[1]?.Value, out int existingDays))
                     {
                         log.LogWarning($"Couldn't parse the days out of existing entry {item.Content}");
                         continue;
                     }
-                    int calculatedDays = workWeekOnly ?? false ? workDays : days;
-                    log.LogInformation($"Checking existing entries for changes. Comparing existing {existingDays} days to calculated {calculatedDays} days");
-                    if ((existingDays == calculatedDays) && !forceWrite)
+                    log.LogInformation($"Checking existing entries for changes. Comparing existing {existingDays} days to calculated {days} days");
+                    if ((existingDays == days) && !forceWrite)
                     {
                         log.LogInformation($"Skipping entry as days don't need update. Entry is: {item.Content}");
                         continue;
@@ -90,14 +76,7 @@ public class ProcessTodoist
                 }
             }
 
-            string daysDisplay = workWeekOnly switch
-            {
-                true => workDays.ToString(),
-                false => days.ToString(),
-                _ => $"{days}/{workDays}"
-            };
-            string update = GetUpdateText(workWeekOnly, days, workDays, daysDisplay);
-            log.LogInformation($"{item.Content}: {nameof(GetUpdateText)} returned {update}");
+            string update = $" [{days}/{workDays} days remaining]";
             log.LogInformation($"{item.Content}, now: {DateTime.Now.Date} to due date: {item.DueDate.Date} is {days}/{workDays}");
             if (Regex.IsMatch(item.Content, regex))
             {
@@ -115,35 +94,11 @@ public class ProcessTodoist
             // which when in BST changes the due date back to the day before (at 11pm). Therefore override this by creating a new instance.
             string dueDateString = item.DueDate.Date.Value.ToString("yyyy-MM-dd");
             item.DueDate = new DueDate(dueDateString, null, item.DueDate.Language);
-#if DEBUG
             log.LogInformation($"{item.Content} with due date: {dueDateString}");
-#else
-                log.LogInformation("Writing to Todoist");
-                await client.Items.UpdateAsync(item);
+#if !DEBUG
+            await client.Items.UpdateAsync(item);
 #endif
         }
-    }
-
-    private static string GetUpdateText(bool? workWeekOnly, int days, int workDays, string daysDisplay)
-    {
-        string update = $" [{daysDisplay} days remaining]";
-
-        if (!(workWeekOnly ?? false)) // days will always be bigger than workdays, so only blank string on days consideration if workWeekOnly == null
-        {
-            if (days <= 0)
-            {
-                update = string.Empty;
-            }
-        }
-        else
-        {
-            if (workDays <= 0)
-            {
-                update = string.Empty;
-            }
-        }
-
-        return update;
     }
 
     private static async Task<List<Item>> GetTodoistProjectItems(ILogger log, ITodoistClient client, List<ComplexId> tdiProjectIds)
@@ -185,7 +140,7 @@ public class ProcessTodoist
     /// <returns>tuple of total days, work days</returns>
     public static (int days, int workDays) CalculateDays(DateTime date)
     {
-        if (DateTime.Now.Date >= date)
+        if (DateTime.Now.Date >= date.Date)
         {
             return (0, 0);
         }
@@ -194,7 +149,7 @@ public class ProcessTodoist
         int workDays = Enumerable
             .Range(1, days - 1)  // days-1 since we don't want to count the final day if it's a workday
             .Select(x => DateTime.Now.Date.AddDays(x))
-            .Count(x => x.DayOfWeek != System.DayOfWeek.Saturday && x.DayOfWeek != System.DayOfWeek.Sunday);
+            .Count(x => x.DayOfWeek is not System.DayOfWeek.Saturday and not System.DayOfWeek.Sunday);
 
         return (days, workDays);
     }
@@ -202,14 +157,13 @@ public class ProcessTodoist
     /// <summary>
     /// Get nullable bool from environment variable
     /// </summary>
-    private static bool? GetBoolFromEnvVar(string envVarName)
+    private static bool GetBoolFromEnvVar(string envVarName)
     {
         string? result = Environment.GetEnvironmentVariable(envVarName);
-        if (result is null)
+        if (result is null || !bool.TryParse(result, out bool envBoolValue))
         {
-            return null;
+            throw new ArgumentException(envVarName);
         }
-        bool.TryParse(result, out bool workWeek);
-        return workWeek;
+        return envBoolValue;
     }
 }
